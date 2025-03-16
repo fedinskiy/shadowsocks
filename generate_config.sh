60e3cf0b0a09a4b5d07d636b7981c99c4a01f8e1
fedinskiy <fdudinskiy@gmail.com>
2025-03-16 20:09:02 +0100

    Simplified the deployment

--- generate_config.sh
+++ generate_config.sh
@@ -7,7 +7,7 @@ set -euo pipefail
 port=${2:?10500}
 version=${3:?1.15.4}
 echo "Enter password"
-read password
+read -s password
 
 echo "Generating configs for $ip:$port"
 # based on https://habr.com/ru/articles/358126/
@@ -35,15 +35,13 @@ cat > client.conf <<EOF
 }
 EOF
 
-folder=$(ssh $1 'pwd')/ssocks
-
 cat > socks.service <<EOF
 [Unit]
 Description=Podman service for shadowsocks
 Documentation=https://github.com/fedinskiy/shadowsocks
 Wants=network-online.target
 After=network-online.target
-RequiresMountsFor=/run/user/1000/containers
+RequiresMountsFor=/run/user/{{uid}}/containers
 
 [Service]
 Environment=PODMAN_SYSTEMD_UNIT=%n
@@ -52,13 +50,12 @@ TimeoutStopSec=70
 ExecStart=/usr/bin/podman start socks
 ExecStop=/usr/bin/podman stop -t 10 socks
 ExecStopPost=/usr/bin/podman stop -t 10 socks
-PIDFile=/run/user/1000/socks-conmon.pid
+PIDFile=/run/user/{{uid}}/socks-conmon.pid
 Type=forking
 
 [Install]
 WantedBy=default.target
 EOF
-
 
 echo "Downloading the binaries of version $version"
 curl -LO https://github.com/shadowsocks/shadowsocks-rust/releases/download/v$version/shadowsocks-v$version.x86_64-unknown-linux-gnu.tar.xz
@@ -67,20 +64,40 @@ curl -LO https://github.com/shadowsocks/shadowsocks-rust/releases/download/v$ver
 sha256sum -c shadowsocks-v$version.x86_64-unknown-linux-gnu.tar.xz.sha256
 tar -xaf shadowsocks-v$version.x86_64-unknown-linux-gnu.tar.xz ssserver sslocal
 
-ssh $1 "rm -r $folder && mkdir $folder"
-scp server.conf socks.service $1:$folder
-
+echo "Creating container"
 podman build -t quay.io/fdudinsk/shadowsocks -f Dockerfile
 podman push quay.io/fdudinsk/shadowsocks
 
-ssh $1 "podman create --name socks -p $port:$port \
---conmon-pidfile=/run/user/1000/socks-conmon.pid \
---pidfile=/run/user/1000/socks.pid \
---mount type=bind,source=$folder/server.conf,target=/server.conf,readonly \
-quay.io/fdudinsk/shadowsocks"
+echo "Gathering data about the server"
+result=$(ssh $1 /bin/bash << 'EOF'
+	folder=$(pwd)/ssocks
+	rm -r $folder || mkdir $folder
+	echo $folder && id -u
+EOF
+)
 
-ssh -t $1 "sudo cp $folder/socks.service /etc/systemd/user/ \
-&& sudo ufw allow $port \
-&& systemctl --user stop socks \
-&& systemctl --user start socks \
-&& systemctl --user enable socks"
+rlt=($result)
+folder=${rlt[0]}
+uid=${rlt[1]}
+
+echo "Copying the configs"
+scp server.conf socks.service $1:$folder
+echo "Starting the server"
+
+ssh -t $1 "sed -ie 's/{{uid}}/${uid}/' ${folder}/socks.service \
+&& sudo cp ${folder}/socks.service /etc/systemd/user/ \
+&& sudo ufw allow $port"
+
+ssh $1 /bin/bash <<EOF
+podman stop socks || podman rm socks
+podman create --name socks -p $port:$port \
+--conmon-pidfile=/run/user/${uid}/socks-conmon.pid \
+--pidfile=/run/user/${uid}/socks.pid \
+--mount type=bind,source=${folder}/server.conf,target=/server.conf,readonly \
+quay.io/fdudinsk/shadowsocks
+
+loginctl enable-linger # to keep server running after logout
+systemctl --user stop socks
+systemctl --user start socks
+systemctl --user enable socks
+EOF
